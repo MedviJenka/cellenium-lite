feaafbaa79788c275fa77e52113333dd3a3cb12d
jenia petrusenko <medvijenia@icloud.com>
2024-06-01 23:56:42 +0300

    update

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,5 @@ RUN apt-get update && \
     apt-get install -y google-chrome-stable vim
 
 COPY . /app
+
 RUN pip install --upgrade pip && pip install -r requirements.txt
--- environment/init_credentials.sh
+++ environment/init_credentials.sh
@@ -2,12 +2,9 @@
 
 # Ensure the credentials file is available in the shared volume
 if [ ! -f /app/credentials/credentials.json ]; then
-  cp /C/Users/evgenyp/Downloads/credentials_1.json /app/credentials/credentials.json
+  cp /c/Users/medvi/Downloads/credentials_1.json /app/credentials/credentials.json
 fi
 
 # Execute the original command
 exec "$@"
-Make sure the script is executable:
-
-
 chmod +x init_credentials.sh
