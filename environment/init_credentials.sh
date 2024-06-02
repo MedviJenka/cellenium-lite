10dfdf4a04c5bbc888134dce1bafa6405a705ebc
Jenia Petrusenko <evgeny.petrusenko@audiocodes.com>
2024-06-02 10:52:12 +0300

    a

--- environment/init_credentials.sh
+++ environment/init_credentials.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 # Ensure the credentials file is available in the shared volume
-if [ ! -f /app/credentials/credentials.json ]; then
-  cp /C/Users/evgenyp/Downloads/credentials_1.json /app/credentials/credentials.json
-fi
+
+cp /C/Users/evgenyp/Downloads/credentials_1.json /app/credentials.json
+
 
 # Execute the original command
 exec "$@"
-Make sure the script is executable:
-
-
 chmod +x init_credentials.sh
