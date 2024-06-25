9c22e1cf8e1dc90c19a6c23b3ed6245ce681a527
Jenia Petrusenko <evgeny.petrusenko@audiocodes.com>
2024-06-25 17:24:57 +0300

    update

--- core/engine/ai_engine.py
+++ core/engine/ai_engine.py
@@ -4,22 +4,20 @@ from dataclasses import dataclass
 from core.manager.reader import read_json
 
 
-@dataclass
 class Binny:
 
-    api_key = read_json(env_key='GPT_API')
-    image_path: str
-    max_tokens: int = 300
+    def __init__(self,*, api_key = read_json(env_key='GPT_API'), max_tokens: int = 1000):
+        self.api_key = api_key
+        self.max_tokens = max_tokens
 
-    def __encode_image(self) -> base64:
-        with open(self.image_path, "rb") as image_file:
+    def __encode_image(self, image_path) -> base64:
+        with open(image_path, "rb") as image_file:
             return base64.b64encode(image_file.read()).decode('utf-8')
 
-    @property
-    def base64_image(self) -> None:
-        return self.__encode_image()
+    def base64_image(self, image_path) -> None:
+        return self.__encode_image(image_path)
 
-    def execute(self, prompt: str) -> None:
+    def execute(self, image_path: str, prompt: str) -> str:
         headers = {
           "Content-Type": "application/json",
           "Authorization": f"Bearer {self.api_key}"
@@ -38,7 +36,7 @@ class Binny:
                 {
                   "type": "image_url",
                   "image_url": {
-                    "url": f"data:image/jpeg;base64,{self.base64_image}"
+                    "url": f"data:image/jpeg;base64,{self.base64_image(image_path)}"
                   }
                 }
               ]
@@ -53,13 +51,31 @@ class Binny:
         return response
 
 
+bini = Binny()
+
+
 def test_user_is_displayed() -> None:
-    bini = Binny(image_path=r"C:\Users\evgenyp\PycharmProjects\cellenium-lite\core\data\images\img.png")
-    response = bini.execute(prompt='is Efrat Lang displayed? and do you see any abnormalities?')
+    response = bini.execute(
+        image_path=r"C:\Users\evgenyp\PycharmProjects\cellenium-lite\core\data\images\img.png",
+        prompt='is Efrat Lang displayed on the right of the screen? and do you see any abnormalities?')
     assert 'Yes' in response
 
 
+def test_outgoing_calls_under_external_p2p() -> None:
+    response = bini.execute(
+        image_path=r"C:\Users\evgenyp\PycharmProjects\cellenium-lite\core\data\images\img_3.png",
+        prompt='how many Outgoing" has the call type "External p2p')
+    assert '3' or 'Three' in response
+
+
 def test_count_rows() -> None:
-    bini = Binny(image_path=r"C:\Users\evgenyp\PycharmProjects\cellenium-lite\core\data\images\img_1.png")
-    response = bini.execute(prompt='count all the rows that starts with blue play button')
-    assert '10' in response
+    response = bini.execute(
+        image_path=r"C:\Users\evgenyp\PycharmProjects\cellenium-lite\core\data\images\img_1.png",
+        prompt='count all the rows that starts with blue play button, expected are 10')
+    assert '10' or 'ten' in response
+
+
+def test_no_rows() -> None:
+    response = bini.execute(image_path=r"C:\Users\evgenyp\PycharmProjects\cellenium-lite\core\data\images\img_2.png",
+                            prompt='are there any rows?, expected is no')
+    assert 'No rows' in response
