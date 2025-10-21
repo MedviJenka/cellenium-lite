9208cc3c2a6d7a84fd82e86b86f8b55239df9559
Jenia Petrusenko <medvijenka@users.noreply.github.com>
2025-10-22 01:02:58 +0300

    fix

--- infrastructure/senior/ui.py
+++ infrastructure/senior/ui.py
@@ -17,7 +17,7 @@ def driver() -> Generator[DriverEngine, None, None]:
 
 class TestLogin:
 
-    screen = "https://the-internet.herokuapp.com/login"
+    url = "https://the-internet.herokuapp.com/login"
 
     @pytest.fixture(autouse=True)
     def setup(self, driver: DriverEngine):
@@ -27,7 +27,7 @@ class TestLogin:
     @pytest.mark.parametrize('password', ['wrong', 'SuperSecretPassword'])
     def test_login_flow(self, username: str, password: str) -> None:
         """Elements are mapped from Google Sheets"""
-        self.engine.get_web(self.screen)
+        self.engine.get_web(self.url)
         self.engine.get_element('username_field').send_keys(username)
         self.engine.get_element('password_field').send_keys(password)
         self.engine.get_element('submit').click()
