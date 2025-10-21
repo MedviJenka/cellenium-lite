5432dbf9496f2138a308bb30ccf2e563bb49e782
Jenia Petrusenko <medvijenka@users.noreply.github.com>
2025-10-22 00:37:03 +0300

    fixed google sheet

--- infrastructure/engine/api_engine.py
+++ infrastructure/engine/api_engine.py
@@ -39,13 +39,13 @@ def __read_google_sheet(sheet_name: str, value: str, api: GoogleAPIAuth) -> dict
     all_rows = sheet.get_all_values()
     headers = all_rows[0]
 
-    log.fire.info(sheet)
-    log.fire.info(all_rows)
-    log.fire.info(headers)
+    log.fire.info(f'sheet: {sheet}')
+    log.fire.info(f'rows: {all_rows}')
+    log.fire.info(f'headers: {headers}')
 
     for row in all_rows[1:]:
         row_dict = dict(zip(headers, row))
-        log.fire.log_info(row_dict)
+        log.fire.info(f'{row_dict}')
         if row_dict['name'] == value:
             return row_dict
 
--- infrastructure/senior/ui.py
+++ infrastructure/senior/ui.py
@@ -20,7 +20,7 @@ class TestLogin:
     screen = "https://the-internet.herokuapp.com/login"
 
     @pytest.fixture(autouse=True)
-    def setup_class(self, driver: DriverEngine):
+    def setup(self, driver: DriverEngine):
         self.engine = driver
 
     @pytest.mark.parametrize('username', ['wrong', 'tomsmith'])
@@ -28,7 +28,6 @@ class TestLogin:
     def test_login_flow(self, username: str, password: str) -> None:
         """Elements are mapped from Google Sheets"""
         self.engine.get_web(self.screen)
-
         self.engine.get_element('username_field').send_keys(username)
         self.engine.get_element('password_field').send_keys(password)
         self.engine.get_element('submit').click()
